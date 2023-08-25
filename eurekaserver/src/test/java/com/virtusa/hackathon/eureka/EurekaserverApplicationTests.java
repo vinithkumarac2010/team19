37d6a22d332361f3d0e66a379786cdb9b7ce6739
vinithkumarac2010 <vinithkumarac2010@gmail.com>
2023-08-25 20:12:02 +0530

    Added

---
+++ eurekaserver/src/test/java/com/virtusa/hackathon/eureka/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.hackathon.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
